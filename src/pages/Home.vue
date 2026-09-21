bfaca659877cb910b0bed2f437b04c7efa8891ce
SelvanivasK <selvanivaskcse@ssmiet.ac.in>
2026-09-21 22:40:20 +0530

    the Home page is added

---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="p-6">
+		<h1 class="text-2xl font-semibold">
+			Frappe Vue UI Starter
+		</h1>
+
+		<p class="mt-2 text-gray-600">
+			Your Frappe UI frontend is working.
+		</p>
+	</div>
+</template>

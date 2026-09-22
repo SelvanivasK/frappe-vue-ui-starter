f3425ef94200f7744c48aa8e9c229d063db28e77
SelvanivasK <selvanivaskcse@ssmiet.ac.in>
2026-09-22 21:29:45 +0530

    updated Home Login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="p-10">
-    <h1 class="text-3xl font-bold">Hello from Vue + Frappe UI 🚀</h1>
-    <Button theme="gray" variant="solid" icon-left="code"> Click me </Button>
+  <div>
+    <router-view />
   </div>
-
 </template>
-
-<script setup>
-import { Button } from "frappe-ui";
-
-</script>
